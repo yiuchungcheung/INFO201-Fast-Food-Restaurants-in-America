78be5d01a28beb858bed2b192e8aec1d9f282413
yiuchungcheung <yiuchungc0730@gmail.com>
2020-02-29 12:39:31 -0800

    interactive map

---
+++ scripts/firstchart.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(lintr)
+library(styler)
+library(tidyr)
+library(leaflet)
+library(ggplot2)
+library(plotly)
+
+data <- read.csv(file = "data/Datafiniti_Fast_Food_Restaurants_May19.csv", stringsAsFactors = FALSE)
+
+# interactive map 
+map <- leaflet(data = data) %>%
+  addTiles() %>%
+  addCircleMarkers(
+    lat = ~latitude,
+    lng = ~longitude,
+    radius = 0.005, 
+    popup = paste(
+      "Name:", data$name, "<br>",
+      "City:", data$city, "<br>",
+      "Address:", data$address, "<br>",
+      "State:", data$province, "<br>",
+      "Category:", data$categories, "<br>"
+    )
+  )

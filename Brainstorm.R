8aaac7d599b0cba9b305bb9496be924412b5e863
Anousha <anshady@uw.edu>
2020-03-01 02:10:51 -0800

    directory fixes

--- Brainstorm.R
+++
@@ -1,8 +0,0 @@
-
-
-#https://www.kaggle.com/GoogleNewsLab/health-searches-us-countyRegionalInterestByConditionOverTime.csv
-
-examination_results <- read.csv("examination.csv", stringsAsFactors = FALSE)
-
-medication <- read.csv("medications.csv", stringAsFactors = FALSE)
-

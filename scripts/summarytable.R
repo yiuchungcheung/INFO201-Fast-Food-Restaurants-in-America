8ce12c614ba021348f78a398f7343055462d6ff3
kshruti99 <shrutikompella@gmail.com>
2020-03-02 11:40:39 -0800

    summary calc done

---
+++ scripts/summarytable.R
@@ -0,0 +1,28 @@
+library(dplyr)
+library(lintr)
+library(styler)
+library(tidyr)
+library(leaflet)
+library(ggplot2)
+library(plotly)
+
+data <- read.csv(file = "data/Datafiniti_Fast_Food_Restaurants_May19.csv", stringsAsFactors = FALSE)
+
+by_state <- data %>%
+  group_by(province) %>%
+  summarise(n_fast_food = n()) %>%
+  select(province, n_fast_food)
+
+by_mcd <- data %>%
+  filter(name == "McDonald's") %>%
+  group_by(province) %>%
+  summarise(num_mcd = n()) %>%
+  select(province, num_mcd)
+
+most_pop_rest <- data %>%
+  group_by(name) %>%
+  summarise(num_rest = n()) %>%
+  top_n(3, wt = num_rest) %>%
+  select(name)
+
+state_fast_food_places <- as.data.frame(by_state)

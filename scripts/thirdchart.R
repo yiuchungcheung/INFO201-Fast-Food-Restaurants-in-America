d34c017e143e410bbe358704b941937491ec3056
mmk42 <52052848+mmk42@users.noreply.github.com>
2020-03-02 15:54:40 -0800

    finished bar graph

--- scripts/thirdchart.R
+++ scripts/thirdchart.R
@@ -12,16 +12,39 @@ data <- read.csv(file = "data/Datafiniti_Fast_Food_Restaurants_May19.csv",
 
 # bar chart - grouped by 
 num_per_name_df <- data %>%
-  group_by(name) %>%
+  group_by(name = tolower(name)) %>%
   summarize(count = n()) %>%
   top_n(10, count) %>%
   arrange(desc(count))
-  
 
+#df without the rop 10
+not_top_10 <- data %>%
+  group_by(name = tolower(name)) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count)) %>%
+  slice(11: n())
+
+# df without the top 10 
+num_per_name_not_top_10_df <- data %>%
+  group_by(name = tolower(name)) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count)) %>%
+  slice(11: n()) %>%
+  summarize(sum = sum(count))
+
+
+num_per_name_df <- rbind(num_per_name_df, Other = c(num_per_name_not_top_10_df$sum))
+
+num_per_name_df$name[num_per_name_df$name == num_per_name_not_top_10_df$sum] <- "Other"
+
+num_per_name_df <- num_per_name_df %>%
+  arrange(desc(count))
+
+# create the bar chart
 bar_chart <- ggplot(data = num_per_name_df) +
   geom_col(mapping = aes(x = name, y = count)) +
   ggtitle("Top Ten Fast Food Restaurants") +
-  labs(x = "Name", y ="Total Number of Restaurants") +
+  labs(x = "Name", y ="Number of Restaurants") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 5))
 
 bar_chart <- ggplotly(bar_chart)

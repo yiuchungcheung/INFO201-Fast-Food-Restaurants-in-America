92711308ea5bcbf1ecef1ecae89f8959c13990c5
mmk42 <52052848+mmk42@users.noreply.github.com>
2020-03-02 17:11:53 -0800

    attempt at pie chart

--- scripts/fourthchart.R
+++ scripts/fourthchart.R
@@ -19,17 +19,40 @@ num_per_name_df <- data %>%
   top_n(10, count) %>%
   arrange(desc(count))
 
+
 # create the bar chart
-bar_chart <- ggplot(data = num_per_name_df) +
-  geom_col(mapping = aes(x = name, y = count)) +
-  ggtitle("Top Ten Fast Food Restaurants") +
-  labs(x = "Name", y ="Number of Restaurants") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 5))
+#bar_chart <- ggplot(data = num_per_name_df) +
+#  geom_col(mapping = aes(x = name, y = count)) +
+#  ggtitle("Top Ten Fast Food Restaurants") +
+#  labs(x = "Name", y ="Number of Restaurants") +
+#  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 5))
 
 # create a pie chart
-bar_chart <- ggplot(data = num_per_name_df, aes(x="", y=count, fill = name)) +
-  geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start=0)
+#bar_chart <- ggplot(data = num_per_name_df, aes(x="", y=count, fill = name)) +
+#  geom_bar(stat="identity", width=1, color="white") +
+#  coord_polar("y", start=0) +
+#  ggtitle("Top Ten Fast Food Restaurants") +
+#  theme_void() #+
+  #geom_text(aes(y = lab.ypos, label = count), color = "white") +
+  #scale_fill_manual(values = mycols)
+  # theme(legend.position="none")
+
+# referenced from this site : https://plot.ly/r/pie-charts/
+bar_chart <- plot_ly(data = num_per_name_df, labels = ~name, values = ~count, type = 'pie',
+               textposition = 'inside',
+               textinfo = 'label+percent',
+               insidetextfont = list(color = '#FFFFFF'),
+               hoverinfo = 'text',
+               text = ~paste(count, ' stores'),
+               marker = list(colors = colors,
+                             line = list(color = '#FFFFFF', width = 1)),
+               showlegend = FALSE)
+
+bar_chart <- bar_chart %>% layout(title = 'Top Ten Fast Food Restaurants',
+                                  xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklables = FALSE), 
+                                  yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklables = FALSE))
+
+
 
 bar_chart <- ggplotly(bar_chart)
 

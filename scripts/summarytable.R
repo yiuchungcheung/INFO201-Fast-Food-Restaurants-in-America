366c653f995fcae8023a80ca8f4a932c20f38349
yiuchungcheung <yiuchungc0730@gmail.com>
2020-03-02 23:17:55 -0800

    summarytable lint error fixes

--- scripts/summarytable.R
+++ scripts/summarytable.R
@@ -8,9 +8,16 @@ library(plotly)
 
 summary_table <- function(data) {
   by_state <- data %>%
-    mutate(isMcD = ifelse(name == "McDonald's" | name == "McDonalds", 1, 0)) %>%
+    mutate(isMcD = ifelse(name == "McDonald's" |
+      name == "McDonalds", 1, 0)) %>%
     group_by(province) %>%
-    summarise(n_fast_food = n(), n_mcd = sum(isMcD), n_mcd_proportion = paste0(round((n_mcd/n_fast_food)*100),"%")) %>%
+    summarise(
+      n_fast_food = n(), n_mcd = sum(isMcD),
+      n_mcd_proportion = paste0(
+        round((n_mcd / n_fast_food) * 100),
+        "%"
+      )
+    ) %>%
     select(province, n_fast_food, n_mcd, n_mcd_proportion)
 
   state_fast_food_places <- as.data.frame(by_state)

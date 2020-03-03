7e01bfe72fa0e830c08c9f97ef475af5681f15ab
Anousha <anshady@uw.edu>
2020-03-02 23:41:09 -0800

    Titling

--- scripts/secondchart.R
+++ scripts/secondchart.R
@@ -18,7 +18,7 @@ second_chart <- function(data) {
 
   bar_chart <- ggplot(data = total_fast_food_chains) +
     geom_col(mapping = aes(x = province, y = total_num_restaurants)) +
-    # ggtitle("Total Number of Fast Food Restaurants per State ") +
+    ggtitle("Total Number of Fast Food Restaurants per State ") +
     labs(x = "State (Abr.)", y = "Total Number of Restaurants") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 5))
 

be92fd39a0ed4945da4c31f10eb8f577bfc361db
kshruti99 <shrutikompella@gmail.com>
2020-03-02 16:55:24 -0800

    changed summary function

--- scripts/summarytable.R
+++ scripts/summarytable.R
@@ -6,11 +6,12 @@ library(leaflet)
 library(ggplot2)
 library(plotly)
 
-data <- read.csv(file = "data/Datafiniti_Fast_Food_Restaurants_May19.csv", stringsAsFactors = FALSE)
-
-by_state <- data %>%
-  group_by(province) %>%
-  summarise(n_fast_food = n()) %>%
-  select(province, n_fast_food)
-
-state_fast_food_places <- as.data.frame(by_state)
+summary_table <- function(data) {
+  by_state <- data %>%
+    group_by(province) %>%
+    summarise(n_fast_food = n()) %>%
+    select(province, n_fast_food)
+  
+  state_fast_food_places <- as.data.frame(by_state)
+  return (state_fast_food_places)
+}

342d92ace59f4f20fd138cf58bdc1677c62de455
kshruti99 <shrutikompella@gmail.com>
2020-03-02 22:44:17 -0800

    fixed summary table

--- scripts/summarytable.R
+++ scripts/summarytable.R
@@ -8,9 +8,10 @@ library(plotly)
 
 summary_table <- function(data) {
   by_state <- data %>%
+    mutate(isMcD = ifelse(name == "McDonald's" | name == "McDonalds", 1, 0)) %>%
     group_by(province) %>%
-    summarise(n_fast_food = n()) %>%
-    select(province, n_fast_food)
+    summarise(n_fast_food = n(), n_mcd = sum(isMcD)) %>%
+    select(province, n_fast_food, n_mcd)
   
   state_fast_food_places <- as.data.frame(by_state)
   return (state_fast_food_places)

236f398eb3fe592522372ec9f5f2dd917039d32a
kshruti99 <shrutikompella@gmail.com>
2020-03-02 23:11:18 -0800

    updated sum table

--- scripts/summarytable.R
+++ scripts/summarytable.R
@@ -10,8 +10,8 @@ summary_table <- function(data) {
   by_state <- data %>%
     mutate(isMcD = ifelse(name == "McDonald's" | name == "McDonalds", 1, 0)) %>%
     group_by(province) %>%
-    summarise(n_fast_food = n(), n_mcd = sum(isMcD)) %>%
-    select(province, n_fast_food, n_mcd)
+    summarise(n_fast_food = n(), n_mcd = sum(isMcD), n_mcd_proportion = paste0(round((n_mcd/n_fast_food)*100),"%")) %>%
+    select(province, n_fast_food, n_mcd, n_mcd_proportion)
 
   state_fast_food_places <- as.data.frame(by_state)
   return(state_fast_food_places)

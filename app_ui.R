e4dcb5ad989eefedb87b5d0384d6cac3d2ac564f
Anousha <anshady@uw.edu>
2020-03-10 22:47:40 -0700

    Proof read intro and reformatted

--- app_ui.R
+++ app_ui.R
@@ -5,14 +5,6 @@
 intro_panel <- 
   mainPanel(
     includeHTML("intro_page.html"),
-    # h1("United State's Fast Food Distribution Project", align = "center"),
-    # #img(src = "Cartoon_fast_food"),
-    # h3("Topic Inspiration"),
-    # HTML("<p> Info</p>
-    #     <br>
-    #     <p>Info</p>"
-         
-    # )
   )
 
 # Map of most popular fast food places
